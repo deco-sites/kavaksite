c86c7e7799dabed5b311d95b70559a2fd8b1865b
Rafael-Oliveira7 <rafasoli10@gmail.com>
2023-07-17 19:35:13 -0300

    fix: route-app

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,21 @@
 import { AppProps } from "$fresh/server.ts";
 import GlobalTags from "$store/components/GlobalTags.tsx";
 import Theme from "$store/sections/Theme/Theme.tsx";
+import GoogleTagManager from "partytown/integrations/GTM.tsx";
+import { context } from "$live/live.ts";
+
+const trackingId = "";
 
 function App(props: AppProps) {
   return (
     <>
       {/* Include default fonts and css vars */}
       <Theme />
+
+      {/* Add Tag Manager script during production only. To test it locally remove the condition */}
+      {!!context.deploymentId && trackingId && (
+        <GoogleTagManager trackingId={trackingId} />
+      )}
 
       {/* Include Icons and manifest */}
       <GlobalTags />
