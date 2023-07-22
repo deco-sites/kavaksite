d3e3d318028dac438227f3cb1e17276f76d25bff
Rafael-Oliveira7 <rafasoli10@gmail.com>
2023-07-22 14:33:44 -0300

    Revert "feat: trazendo butterCMS para o projeto"
    This reverts commit 927ff6cf0d8b0c009fe06d147d5d461223e992fc.

--- components/blog/gallery/GalleryPagination.tsx
+++ components/blog/gallery/GalleryPagination.tsx
@@ -1,6 +1,6 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import Icon from "../../../components/Icon.tsx";
-import { Pagination } from "deco-sites/kavaksite/std/butterCMS/types.ts";
+import { Pagination } from "deco-sites/std/commerce/butterCMS/types.ts";
 import { useLayoutEffect } from "preact/compat";
 
 export interface Props {
