770cfb78787f3c0e7dbf47d959f3a3a8f82ae61f
Rafael-Oliveira7 <rafasoli10@gmail.com>
2023-07-23 01:47:35 -0300

    feat: news screens and color primary

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,5 +4,16 @@ export default {
   plugins: [daisyui],
   daisyui: { themes: [], logs: false },
   content: ["./**/*.tsx"],
-  theme: { container: { center: true } },
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3374db",
+      },
+      screens: {
+        "2lg": "1200px",
+        "1.5xl": "1400px",
+      },
+    },
+    container: { center: true },
+  },
 };
