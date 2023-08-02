c9f92886a5ea67547bd21b88be48fdc1083ff3b1
Gabriel Collo <gcollo46@gmail.com>
2023-08-02 11:50:02 -0300

    fix: hide empty fields blog places

--- components/blog/sections/BlogPlaces.tsx
+++ components/blog/sections/BlogPlaces.tsx
@@ -14,7 +14,7 @@ export function BlogPlaces({ title, places }: BlogSectionPlaces) {
               href={place.slug.includes("https")
                 ? place.slug
                 : `/${place.slug}`}
-              class="text-sm text-gray-light leading-[1] px-4 py-2 border rounded-md border-gray block hover:bg-primary hover:text-white duration-300 hover:duration-300 hover:border-primary"
+              class="text-sm text-gray-light leading-[1] px-4 py-2 border rounded-md border-gray block hover:bg-primary hover:text-white duration-300 hover:duration-300 hover:border-primary empty:hidden"
             >
               {place.name}
             </a>
